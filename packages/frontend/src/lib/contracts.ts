184c8e9c303f26f0463452093d5489c47b63278c
Keerthikanth Rohit. A <rohitkeerthikanth@gmail.com>
2025-12-07 16:15:21 +0530

    added new functions to abis

--- packages/frontend/src/lib/contracts.ts
+++ packages/frontend/src/lib/contracts.ts
@@ -67,6 +67,13 @@ export const VAULT_ABI = [
     type: "function",
   },
   {
+    inputs: [{ internalType: "address", name: "user", type: "address" }],
+    name: "userGrowthPercent",
+    outputs: [{ internalType: "int256", name: "", type: "int256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "totalSupply",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
@@ -122,6 +129,16 @@ export const ROUTER_ABI = [
 ] as const;
 
 export const ERC20_ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      { indexed: false, internalType: "uint256", name: "value", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
   {
     inputs: [{ internalType: "address", name: "account", type: "address" }],
     name: "balanceOf",

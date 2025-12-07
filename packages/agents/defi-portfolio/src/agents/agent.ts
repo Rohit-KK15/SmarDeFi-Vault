55b632ab1448b995caf4f63072b72525b51b6eab
Keerthikanth Rohit. A <rohitkeerthikanth@gmail.com>
2025-12-07 13:09:14 +0530

    minor prompt fix

--- packages/agents/defi-portfolio/src/agents/agent.ts
+++ packages/agents/defi-portfolio/src/agents/agent.ts
@@ -11,7 +11,7 @@ export const getRootAgent = async () => {
         .withDescription("AI Agent that monitors and manages the Funds and Strategies of the Vault.")
         .withInstruction(`
             Use the sub-agent Strategy Sentinel Agent for getting vault, strategies, and users' stats, and perform deposit, withdraw, rebalance, harvest, auto_deleverage.
-            Use the sub-agent Yield Simulator Agent for accruing or generating yeild to the Pool.
+            Use the sub-agent Yield Generator Agent for accruing or generating yeild to the Pool.
             `)
         .withModel(model)
         .withSubAgents([strategySentinalAgent, yieldSimulatorAgent])
